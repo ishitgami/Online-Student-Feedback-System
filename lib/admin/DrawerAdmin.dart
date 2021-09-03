39b8274bea372a4e69dcfef9b81750ac82ac4776
unknown <ishitgami01@gmail.com>
2021-09-03 10:07:37 +0530

    Change uI of Admin Drawer

--- lib/admin/DrawerAdmin.dart
+++ lib/admin/DrawerAdmin.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import '../route.dart';
 
 
@@ -12,15 +11,19 @@ class AdminDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      
       child: SafeArea(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            _createHeader(),
+            // _createHeader(),
             ListTile(
               title: Row(
                 children: [
-                  Icon(Icons.dashboard,color: Colors.blue,),
+                  Icon(
+                    Icons.dashboard,
+                    color: Colors.blue,
+                  ),
                   SizedBox(
                     width: 30,
                   ),
@@ -29,42 +32,154 @@ class AdminDrawer extends StatelessWidget {
               ),
               onTap: () {
                 Navigator.pop(context);
-                Navigator.pushNamed(context, AdminScreenRoute);
+                Navigator.pushNamed(context,AdminScreenRoute);
               },
             ),
-            SizedBox(
-              width: 50,
-              child: Divider()),
+            SizedBox(width: 50, child: Divider()),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 5),
+              child: Text(
+                'COLLAGE',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
             ListTile(
-              title: Text('Academic Year'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.calendarAlt,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Academic Year'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
-                Navigator.pushNamed(context, acadamicYearScreenRoute);
+                Navigator.pushNamed(context,acadamicYearScreenRoute);
               },
             ),
             ListTile(
-              title: Text('DepartMent'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.networkWired,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('DepartMent'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, departmentScreenRoute);
               },
             ),
             ListTile(
-              title: Text('Division'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.users,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Division'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, divisionScreenRoute);
               },
             ),
             ListTile(
-              title: Text('Subject'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.book,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Subject'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, subjectScreenRoute);
               },
             ),
+             ListTile(
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.chalkboardTeacher,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Faculty'),
+                ],
+              ),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, facultyInAdminScreenRoute);
+              },
+            ),
             ListTile(
-              title: Text('Feedback Question'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.userGraduate,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Student'),
+                ],
+              ),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, studentInAdminScreenRoute);
+              },
+            ),
+            SizedBox(width: 50, child: Divider()),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 5),
+              child: Text(
+                'FEEDBACK',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            ListTile(
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.quora,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Question'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, questionScreenRoute);
@@ -73,8 +188,14 @@ class AdminDrawer extends StatelessWidget {
             ListTile(
               title: Row(
                 children: [
-                  Expanded(child: Text('Feedback Restrictions')),
-                  Icon(Icons.adjust),
+                  Icon(
+                    FontAwesomeIcons.server,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(child: Text('Restrictions')),
                 ],
               ),
               onTap: () {
@@ -83,59 +204,28 @@ class AdminDrawer extends StatelessWidget {
               },
             ),
             ListTile(
-              title: Text('Feedback Report'),
+              title: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.filePdf,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text('Report'),
+                ],
+              ),
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, reportScreenRoute);
               },
             ),
-            ListTile(
-              title: Text('Faculty'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, facultyInAdminScreenRoute);
-              },
-            ),
-            ListTile(
-              title: Text('Student'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, studentInAdminScreenRoute);
-              },
-            ),
+            SizedBox(width: 50, child: Divider()),
+           
           ],
         ),
       ),
     );
   }
-}
-
-Widget _createHeader() {
-  return DrawerHeader(
-      margin: EdgeInsets.zero,
-      padding: EdgeInsets.zero,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('img/drawerbg.jpg'))),
-      child: Stack(children: <Widget>[
-        Positioned(
-            bottom: 12.0,
-            left: 16.0,
-            child: Row(
-              children: [
-                 Text(
-                  "Admin",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 38.0,
-                      fontWeight: FontWeight.w700), 
-                ),
-                SizedBox(
-                  width: 100,
-                ),
-                Icon(FontAwesomeIcons.userCog,size: 50),
-              ],
-            )),
-      ]));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
-  import 'package:flutter/material.dart';
-  import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:osfs1/route.dart';
 
-  import 'package:osfs1/route.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(OSFS());
+}
 
-
-  void main() async{  
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-    runApp(OSFS());
-    } 
-
-  class OSFS extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-         onGenerateRoute: Routerr.generateRoute,
+class OSFS extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: Routerr.generateRoute,
       initialRoute: loginScreenRoute,
-      );
-    }
+    );
   }
+}

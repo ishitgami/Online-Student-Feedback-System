782c6d8f11927acbae926afe18e9eaa5ee564fea
unknown <ishitgami01@gmail.com>
2021-11-13 21:10:47 +0530

    Create Faculty DashBord

--- lib/faculty/FacultyDashbord.dart
+++ lib/faculty/FacultyDashbord.dart
@@ -5,11 +5,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 
 class FacultyDashbordScreen extends StatefulWidget {
-
   @override
   _FacultyDashbordScreenState createState() => _FacultyDashbordScreenState();
 }
@@ -18,6 +16,9 @@ class _FacultyDashbordScreenState extends State<FacultyDashbordScreen> {
   final auth = FirebaseAuth.instance;
   User user;
   var uid;
+  final Color dark = const Color(0xff3b8c75);
+  final Color normal = const Color(0xff64caad);
+  final Color light = const Color(0xff73e8c9);
 
   @override
   void initState() {
@@ -25,114 +26,240 @@ class _FacultyDashbordScreenState extends State<FacultyDashbordScreen> {
     user = auth.currentUser;
     uid = user.uid;
   }
+
   @override
   Widget build(BuildContext context) {
-    const cutOffYValue = 0.0;
-    const yearTextStyle =
-    TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold);
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       drawer: FacultyDrawer(),
-      appBar: AppBar(
-          title: Text('DashBoard'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.logout,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                FirebaseFirestore.instance.clearPersistence();
-                FirebaseAuth.instance.signOut();
-                Navigator.pushReplacementNamed(context, loginScreenRoute);
-              },
-            )
-          ]),
-      body: SizedBox(
-      width: 330,
-      height: 180,
-      child: LineChart(
-        LineChartData(
-          lineTouchData: LineTouchData(enabled: false),
-          lineBarsData: [
-            LineChartBarData(
-              spots: [
-                FlSpot(0, 0),
-                FlSpot(1, 1),
-                FlSpot(2, 3),
-                FlSpot(3, 3),
-                FlSpot(4, 5),
-                FlSpot(4, 4)
-              ],
-              isCurved: false,
-              barWidth: 1,
-              colors: [
-                Colors.black,
-              ],
-              belowBarData: BarAreaData(
-                show: true,
-                colors: [Colors.lightGreen.withOpacity(0.4)],
-                cutOffY: cutOffYValue,
-                applyCutOffY: true,
-              ),
-              aboveBarData: BarAreaData(
-                show: true,
-                colors: [Colors.red.withOpacity(0.6)],
-                cutOffY: cutOffYValue,
-                applyCutOffY: true,
-              ),
-              dotData: FlDotData(
-                show: false,
-              ),
-            ),
-          ],
-          minY: 0,
-          titlesData: FlTitlesData(
-            bottomTitles: SideTitles(
-                showTitles: true,
-                reservedSize: 6,
-                getTitles: (value) {
-                  switch (value.toInt()) {
-                    case 0:
-                      return '2015';
-                    case 1:
-                      return '2016';
-                    case 2:
-                      return '2017';
-                    case 3:
-                      return '2018';
-                    case 4:
-                      return '2019';
-                    default:
-                      return '';
-                  }
-                }),
-            leftTitles: SideTitles(
-              showTitles: true,
-              getTitles: (value) {
-                return '\$ ${value + 20}';
-              },
-            ),
+      appBar: AppBar(title: Text('DashBoard'), actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.logout,
+            color: Colors.white,
           ),
-          axisTitleData: FlAxisTitleData(
-              leftTitle: AxisTitle(showTitle: true, titleText: 'Value', margin: 10),
-              bottomTitle: AxisTitle(
-                  showTitle: true,
-                  margin: 10,
-                  titleText: 'Year',
-                  textStyle: yearTextStyle,
-                  textAlign: TextAlign.right)),
-          gridData: FlGridData(
-            show: true,
-            checkToShowHorizontalLine: (double value) {
-              return value == 1 || value == 2 || value == 3 || value == 4;
-            },
+          onPressed: () {
+            FirebaseFirestore.instance.clearPersistence();
+            FirebaseAuth.instance.signOut();
+            Navigator.pushReplacementNamed(context, loginScreenRoute);
+          },
+        )
+      ]),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                // width: 500,
+                // height: 200,
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.width * 0.95 * 0.35,
+                padding: EdgeInsets.fromLTRB(10, 10, 40, 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 4.0,
+                      )
+                    ]),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Total',
+                          style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black54),
+                        ),
+                        Text(
+                          'Feedback\nClass',
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Text(
+                      '5',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                // width: 500,
+                // height: 300,
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.width * 0.95 * 0.90,
+                padding: EdgeInsets.fromLTRB(0, 10, 20, 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 4.0,
+                      )
+                    ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(35, 10, 0, 20),
+                      child: Text(
+                        'FeedbackClass Graph',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: BarChart(
+                          BarChartData(
+                            // maxY: 110,
+                            titlesData: FlTitlesData(
+                                show: true,
+                                bottomTitles: SideTitles(
+                                  showTitles: true,
+                                  getTextStyles: (context, value) =>
+                                      const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 10),
+                                ),
+                                topTitles: SideTitles(showTitles: false),
+                                leftTitles: SideTitles(
+                                  showTitles: true,
+                                  getTextStyles: (context, value) =>
+                                      const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 10),
+                                ),
+                                rightTitles: SideTitles(showTitles: false)),
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+
+                            axisTitleData: FlAxisTitleData(
+                              leftTitle: AxisTitle(
+                                  showTitle: true,
+                                  titleText: 'Students',
+                                  textStyle: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold),
+                                  margin: 10),
+                              bottomTitle: AxisTitle(
+                                  showTitle: true,
+                                  titleText: 'Feedback',
+                                  textStyle: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold),
+                                  margin: 10),
+                            ),
+                            barGroups: getData(),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    )
     );
   }
+
+  List<BarChartGroupData> getData() {
+    return [
+      BarChartGroupData(
+        showingTooltipIndicators: [1, 2, 3],
+        x: 1,
+        barsSpace: 4,
+        barRods: [
+          BarChartRodData(
+              y: 150,
+              width: 20,
+              rodStackItems: [
+                BarChartRodStackItem(0, 150, Colors.blueAccent),
+                BarChartRodStackItem(0, 110, Colors.blue[300]),
+              ],
+              borderRadius: const BorderRadius.all(Radius.zero)),
+        ],
+      ),
+      BarChartGroupData(
+        x: 2,
+        barsSpace: 4,
+        barRods: [
+          BarChartRodData(
+              y: 140,
+              width: 20,
+              rodStackItems: [
+                BarChartRodStackItem(0, 140, Colors.blueAccent),
+                BarChartRodStackItem(0, 90, Colors.blue[300]),
+              ],
+              borderRadius: const BorderRadius.all(Radius.zero)),
+        ],
+      ),
+      BarChartGroupData(
+        x: 3,
+        barsSpace: 10,
+        barRods: [
+          BarChartRodData(
+              y: 160,
+              width: 20,
+              rodStackItems: [
+                BarChartRodStackItem(0, 160, Colors.blueAccent),
+                BarChartRodStackItem(0, 140, Colors.blue[300]),
+              ],
+              borderRadius: const BorderRadius.all(Radius.zero)),
+        ],
+      ),
+      BarChartGroupData(
+        x: 4,
+        barsSpace: 4,
+        barRods: [
+          BarChartRodData(
+              y: 100,
+              width: 20,
+              rodStackItems: [
+                BarChartRodStackItem(0, 100, Colors.blueAccent),
+                BarChartRodStackItem(0, 90, Colors.blue[300]),
+              ],
+              borderRadius: const BorderRadius.all(Radius.zero)),
+        ],
+      ),
+      BarChartGroupData(
+        x: 5,
+        barsSpace: 4,
+        barRods: [
+          BarChartRodData(
+              y: 130,
+              width: 20,
+              rodStackItems: [
+                BarChartRodStackItem(0, 130, Colors.blueAccent),
+                BarChartRodStackItem(0, 80, Colors.blue[300]),
+              ],
+              borderRadius: const BorderRadius.all(Radius.zero)),
+        ],
+      ),
+    ];
+  }
 }
-
-
-
